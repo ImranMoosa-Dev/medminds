6f4d261ee1778c98a327230bcbdc8f16b32443f2
shoaibkorai <realshoaibkorai@gmail.com>
2026-04-27 12:56:31 +0500

    added code files

---
+++ MedMindsDB.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql2/promise");
+
+module.exports = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "medminds"
+}).then(connection => {
+    console.log("MySQL Connected");
+    return connection;
+}).catch(err => {
+    console.error("MySQL Connection Error:", err);
+    throw err;
+});
